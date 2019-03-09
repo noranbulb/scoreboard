d894518b12646263df176be583b2f7ccabab90a6
김준 <kimg2153@gmail.com>
2019-03-09 14:37:56 +0900

    pages > scoreboard 폴더 만든고 Scoreboard.js 파일 옮김

---
+++ src/pages/Menu.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+
+import {
+  Collapse, DropdownItem, DropdownMenu, DropdownToggle,
+
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  //NavLink,
+  UncontrolledDropdown
+} from "reactstrap";
+import {NavLink} from "react-router-dom";
+
+export class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="md">
+          <NavLink to="/" className="navbar-brand">react</NavLink>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink to="/heroes" className="nav-link">heroes</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to="/scoreboard" className="nav-link">scoreboard</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+
+    )
+  }
+}
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,8 +1,76 @@
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import {Home} from "./Home";
+import {Heroes} from "./Heroes";
+import {Scoreboard} from "./scoreboard/Scoreboard";
+import {Menu} from "./Menu";
+
+
+export class Root extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+
+      <BrowserRouter>
+        <Fragment>
+          <Menu/>
+          <p>공통메뉴 영역</p>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+            <Route path="/heroes" component={Heroes}></Route>
+            <Route path="/scoreboard" component={Scoreboard}></Route>
+          </Switch>
+
+        </Fragment>
+
+      </BrowserRouter>
+
+     );
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
 import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {Home} from "./Home";
 import {Heroes} from "./Heroes";
 import {Scoreboard} from "./Scoreboard";
+import {Menu} from "./Menu";
 
 export class Root extends React.Component {
   constructor(props) {
@@ -14,6 +82,7 @@ export class Root extends React.Component {
 
       <BrowserRouter>
         <>
+          <Menu/>
           <p>공통메뉴 영역</p>
           <Switch>
             <Route exact path="/" component={Home}></Route>
@@ -25,7 +94,9 @@ export class Root extends React.Component {
 
       </BrowserRouter>
 
-     );
+    );
 
   }
 }
+
+*/
