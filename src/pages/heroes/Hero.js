eee522b580116eb5adafee1e3db4ce29aef2a55d
김준 <kimg2153@gmail.com>
2019-03-09 17:32:33 +0900

    디테일 페이지 : 동적 유알엘 설정

---
+++ src/pages/heroes/Hero.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export class Hero extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        Hero works.
+      </div>
+
+    )
+  }
+}
